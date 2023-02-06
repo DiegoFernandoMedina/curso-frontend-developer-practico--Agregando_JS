e5a2fd8eee463a5d0499375212051e2223895678
Diego Medina <alejfercho14@gmail.com>
2023-02-06 15:04:29 -0500

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmali = document.querySelector(".navbar-email");
 const desktopMenu= document.querySelector(".desktop-menu");
+const menuHamIcon= document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 menuEmali.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+};
+
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle("inactive");
 };
 
