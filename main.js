df5949ab0343a3bf629931bf1b8004e32acb7785
Diego Medina <alejfercho14@gmail.com>
2023-02-09 14:59:30 -0500

    Se agregaron algunos cambios a la configuración de la lista de productos a partir de arrays

--- main.js
+++ main.js
@@ -6,38 +6,25 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector(".cards-container");
 
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains('inactive');
-
-  if (!isAsideClosed) {
-    aside.classList.add('inactive');
-  }
-  
+  aside.classList.add('inactive');
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains('inactive');
-
-  if (!isAsideClosed) {
-    aside.classList.add('inactive'); 
-  }
-  
+  aside.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside() {
-  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-  
-  if (!isMobileMenuClosed) {
-    mobileMenu.classList.add('inactive'); 
-  }
-  
+  mobileMenu.classList.add('inactive');
   aside.classList.toggle('inactive');
+  desktopMenu.classList.add("inactive");
 }
 
 const productList = [];
@@ -49,22 +36,51 @@ productList.push({
 productList.push({
   name: 'Pantalla',
   price: 220,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  image: 'https://www.lg.com/co/images/monitores/md07564045/gallery/DZ-2.jpg',
 });
 productList.push({
   name: 'Compu',
   price: 620,
+  image: 'https://img.freepik.com/fotos-premium/imagen-fondo-grafico-computadora-portatil-pantalla-blanco-escritorio-contra-fondo-gris-definitivo-maqueta-espacio-copia_236854-28385.jpg',
+});
+productList.push({
+  name: 'Bike',
+  price: 120,
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
+productList.push({
+  name: 'Pantalla',
+  price: 220,
+  image: 'https://www.lg.com/co/images/monitores/md07564045/gallery/DZ-2.jpg',
+});
+productList.push({
+  name: 'Compu',
+  price: 620,
+  image: 'https://img.freepik.com/fotos-premium/imagen-fondo-grafico-computadora-portatil-pantalla-blanco-escritorio-contra-fondo-gris-definitivo-maqueta-espacio-copia_236854-28385.jpg',
+});
+/* 
+<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div> 
+ */
 
 function renderProducts(arr) {
   for (product of arr) {
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+    const productCard = document.createElement('div');//Se crea una etiqueta div
+    productCard.classList.add('product-card');//Se crea una clase para el div llamada product-card
   
     // product= {name, price, image} -> product.image
-    const productImg = document.createElement('img');
-    productImg.setAttribute('src', product.image);
+    const productImg = document.createElement('img');//Se crea una etiqueta img
+    productImg.setAttribute('src', product.image);// Se crea el atributo src para agregar una imagen y se agrega el contenido de la caracteristica del objeto.
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
